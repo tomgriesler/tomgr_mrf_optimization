973b2c1a181ed33c5eb8d305d7e9d29021a85a68
Tom Griesler <tomgr@umich.edu>
2023-11-14 13:40:34 -0500

    start new optimization

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from abdominal_tools import TargetTissue, AcquisitionBlock, store_optimization
 # %% Define possible preparation modules
 prep_modules = ['noPrep', 'TI21', 'TI100', 'TI250', 'TI400', 'T2prep40', 'T2prep80', 'T2prep120']
 prep_module_weights = [1, 1/4, 1/4, 1/4, 1/4, 1/3, 1/3, 1/3]
-total_dur = 10e3
-min_num_preps = 8
+total_dur = 20e3
+min_num_preps = 10
 
 #%% Define target tissue
-target_tissue = TargetTissue(660, 40, 1)
+target_tissue = TargetTissue(1500, 100, 1)
 inversion_efficiency = 0.95
 delta_B1 = 1
 
 #%% Create acquisition block
-acq_block = AcquisitionBlock(np.full(35, 15.), np.full(35, 5.7), 1.4)
+acq_block = AcquisitionBlock(np.full(20, 15.), np.full(20, 20.), 1.0)
 
 #%% Perform optimization
 sequences = optimize_sequence(target_tissue, acq_block, prep_modules, total_dur, prep_module_weights=prep_module_weights, min_num_preps=min_num_preps, N_iter_max=1e6, inversion_efficiency=inversion_efficiency, delta_B1=delta_B1)
