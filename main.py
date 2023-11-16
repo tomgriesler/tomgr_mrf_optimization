ca38b805fbfb041cb26a585d395fdb16adad43e5
Tom Griesler <tomgr@umich.edu>
2023-11-16 15:48:50 -0500

    finish refactoring

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 #%%
-import numpy as np
-
 from optimize_sequence import optimize_sequence
-from abdominal_tools import TargetTissue, AcquisitionBlock, store_optimization
+from abdominal_tools import store_optimization, RESULTSPATH
 
-# %% Define possible preparation modules
+#%% Define optimization target
+target_t1 = 660.
+target_t2 = 40.
+target_m0 = 1.
+
+shots = 35
+const_fa = 15.
+const_tr = 5.7
+te = 1.
+total_dur = 1e4
+
 prep_modules = ['noPrep', 'TI21', 'TI100', 'TI250', 'TI400', 'T2prep40', 'T2prep80', 'T2prep120']
 prep_module_weights = [1, 1/4, 1/4, 1/4, 1/4, 1/3, 1/3, 1/3]
-total_dur = 1e4
 min_num_preps = 8
+n_iter_max = 1e3
 
-#%% Define target tissue
 inversion_efficiency = 0.95
-delta_B1 = 1
+delta_B1 = 1.
+phase_inc = 0.
 
 #%% Perform optimization
-sequences = optimize_sequence(target_tissue, acq_block, prep_modules, total_dur, prep_module_weights=prep_module_weights, min_num_preps=min_num_preps, N_iter_max=1e6, inversion_efficiency=inversion_efficiency, delta_B1=delta_B1)
+sequences = optimize_sequence(target_t1, target_t2, target_m0, shots, const_fa, const_tr, te, total_dur, prep_modules, prep_module_weights, min_num_preps, n_iter_max, inversion_efficiency, delta_B1, phase_inc)
 
 #%%
 prot = {
-    'N': len(sequences),
+    'taget_t1': target_t1,
+    'taget_t2': target_t2,
+    'taget_m0': target_m0,
+    'shots': shots, 
+    'const_fa': const_fa,
+    'const_tr': const_tr,
+    'te': te, 
     'total_dur': total_dur,
     'prep_modules': prep_modules, 
     'prep_module_weights': prep_module_weights,
     'min_num_preps': min_num_preps,
-    'target_tissue': target_tissue.__dict__,
+    'n_iter_max': n_iter_max,
     'inversion_efficiency': inversion_efficiency,
     'delta_B1': delta_B1,
-    'tr_offset': tr_offset
+    'phase_inc': phase_inc
 }
-store_optimization(sequences, prot)
+store_optimization(RESULTSPATH, sequences, prot)
 # %%
