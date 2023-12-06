1b4c0cb04621cc64913eacf8d5bb67541d46afa2
Tom Griesler <tomgr@umich.edu>
2023-12-06 13:50:16 -0500

    update to current implementation

--- create_eventlists_optimization.py
+++ create_eventlists_optimization.py
@@ -4,25 +4,48 @@ from pathlib import Path
 import pickle
 import json
 
-from abdominal_tools import TargetTissue, RESULTSPATH, visualize_sequence, sort_sequences, create_weightingmatrix
+from abdominal_tools import RESULTSPATH, visualize_sequence, sort_sequences, create_weightingmatrix
 
 #%%
-timestamp = '231115_124351'
+timestamp = '231204_090948'
 resultspath = RESULTSPATH/timestamp
 
 with open(resultspath/'sequences.pkl', 'rb') as handle: 
     sequences = pickle.load(handle)
 
-with open(resultspath/'acq_block.pkl', 'rb') as handle: 
-    acq_block = pickle.load(handle)
-
 with open(resultspath/'prot.json', 'r') as handle: 
     prot = json.load(handle)
 
-target_tissue = TargetTissue(prot['target_tissue']['T1'], prot['target_tissue']['T2'], prot['target_tissue']['M0'])
-inv_eff = prot['inv_eff']
+#%%
+for sequence in sequences:
+    sequence.decompress()
+
+#%% old implementation
+target_t1 = prot['target_t1']
+target_t2 = prot['target_t2']
+target_m0 = prot['target_m0']
+shots = prot['shots']
+const_fa = prot['const_fa']
+const_tr = prot['const_tr']
+te = prot['te']
+total_dur = prot['total_dur']
+phase_inc = prot['phase_inc']
+inv_eff = prot['inversion_efficiency']
 delta_B1 = prot['delta_B1']
 
+#%% new implementation
+costfunction = prot['costfunction']
+target_t1 = prot['target_t1']
+target_t2 = prot['target_t2']
+target_m0 = prot['target_m0']
+shots = prot['shots']
+const_fa = prot['const_fa']
+const_tr = prot['const_tr']
+te = prot['te']
+total_dur = prot['total_dur']
+phase_inc = prot['phase_inc']
+inv_eff = prot['inv_eff']
+delta_B1 = prot['delta_B1']
 
 #%%
 weighting = '1/T1, 0, 0'
@@ -30,7 +53,7 @@ weighting = '0, 1/T2, 0'
 weighting = '1/T1, 1/T2, 0'
 
 #%%
-weightingmatrix = create_weightingmatrix(target_tissue, weighting)
+weightingmatrix = create_weightingmatrix(target_t1, target_t2, target_m0, weighting)
 sort_sequences(sequences, weightingmatrix)
 
 #%%
@@ -38,36 +61,14 @@ mrf_seq = sequences[0]
 name = timestamp + '_best_T1T2'
 
 #%%
-waittimes = mrf_seq.waittimes
-
-#%%
-visualize_sequence(mrf_seq, acq_block)
-
-# %%
-FA_FISP = np.tile(acq_block.fa, len(mrf_seq.PREP))
-
-TR_FISP = np.zeros_like(FA_FISP)
-for i, waittime in enumerate(waittimes):
-    TR_FISP[len(acq_block.tr)*(i+1)-1] = waittime * 1e3
-
-PH_FISP = np.zeros_like(FA_FISP)
+visualize_sequence(mrf_seq, show_fa=True)
 
 #%% save lists
-savepath = Path(f'/home/tomgr/Documents/temp/{name}')
+savepath = Path(f'/home/tomgr/Documents/abdominal/MRF_sim_for_tom/Sequences/{name}')
 savepath.mkdir(exist_ok=True)
-np.savetxt(savepath/'PREP_FISP.txt', mrf_seq.PREP, fmt='%i')
-np.savetxt(savepath/'TI_FISP.txt', mrf_seq.TI, fmt='%f')
-np.savetxt(savepath/'T2TE_FISP.txt', mrf_seq.T2TE, fmt='%f')
-np.savetxt(savepath/'FA_FISP.txt', FA_FISP, fmt='%f')
-np.savetxt(savepath/'TR_FISP.txt', TR_FISP, fmt='%f')
-np.savetxt(savepath/'PH_FISP.txt', PH_FISP, fmt='%f')
-
-# %%
-ii = -65
-while True:
-    if 1 in sequences[ii].PREP and 2 in sequences[ii].PREP:
-        print(ii)
-        break
-    else:
-        ii -= 1
-# %%
+np.savetxt(savepath/'PREP_FISP.txt', mrf_seq.prep, fmt='%i')
+np.savetxt(savepath/'TI_FISP.txt', mrf_seq.ti, fmt='%f')
+np.savetxt(savepath/'T2TE_FISP.txt', mrf_seq.t2te, fmt='%f')
+np.savetxt(savepath/'FA_FISP.txt', mrf_seq.fa, fmt='%f')
+np.savetxt(savepath/'TR_FISP.txt', mrf_seq.tr, fmt='%f')
+np.savetxt(savepath/'PH_FISP.txt', mrf_seq.ph, fmt='%f')
--- create_eventlists_reference.py
+++ create_eventlists_reference.py
@@ -2,98 +2,49 @@
 import numpy as np
 from pathlib import Path
 
-from abdominal_tools import MRFSequence, AcquisitionBlock, BLOCKS, visualize_sequence
+from abdominal_tools import MRFSequence, BLOCKS, visualize_sequence
+
 # %%
-# Jaubert mod 4
-name = 'jaubert_mod_10s_4'
-prep_order = ['TI12', 'noPrep', 'T2prep40', 'T2prep80']
-
-# Jaubert mod 8
-name = 'jaubert_mod_10s_8'
-prep_order = ['TI12', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI300', 'noPrep', 'T2prep40']
-
-# Jaubert mod 12
-name = 'jaubert_mod_10s_12'
-prep_order = ['TI12', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI300', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI12', 'noPrep']
-
-# Jaubert mod 16
-name = 'jaubert_mod_10s_16'
-prep_order = ['TI12', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI300', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI12', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI300']
-
-# Jaubert mod 20
-name = 'jaubert_mod_10s_20'
-prep_order = ['TI12', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI300', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI12', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI300', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120']
-
-# Hamilton 4
-name = 'hamilton_10s_4'
-prep_order = ['TI21', 'noPrep', 'T2prep40', 'T2prep80']
-
-# Hamilton 8
-name = 'hamilton_10s_8'
-prep_order = ['TI21', 'noPrep', 'T2prep40', 'T2prep80', 'TI100', 'noPrep', 'T2prep40', 'T2prep80']
-
-# Hamilton 12
-name = 'hamilton_10s_12'
-prep_order = ['TI21', 'noPrep', 'T2prep40', 'T2prep80', 'TI100', 'noPrep', 'T2prep40', 'T2prep80', 'TI250', 'noPrep', 'T2prep40', 'T2prep80']
-
-# Hamilton 16
-name = 'hamilton_10s_16'
-prep_order = ['TI21', 'noPrep', 'T2prep40', 'T2prep80', 'TI100', 'noPrep', 'T2prep40', 'T2prep80', 'TI250', 'noPrep', 'T2prep40', 'T2prep80', 'TI400', 'noPrep', 'T2prep40', 'T2prep80']
-
-# Hamilton 20
-name = 'hamilton_10s_20'
-prep_order = ['TI21', 'noPrep', 'T2prep40', 'T2prep80', 'TI100', 'noPrep', 'T2prep40', 'T2prep80', 'TI250', 'noPrep', 'T2prep40', 'T2prep80', 'TI400', 'noPrep', 'T2prep40', 'T2prep80', 'TI21', 'noPrep', 'T2prep40', 'T2prep80']
-
-# 3D QALAS 4
-name = '3dqalas_10s_4'
-prep_order = ['T2prep50', 'TI100', 'noPrep', 'noPrep']
-
-# 3D QALAS 8
-name = '3dqalas_10s_8'
-prep_order = ['T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep']
-
-# 3D QALAS 12
-name = '3dqalas_10s_12'
-prep_order = ['T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100']
-
-# 3D QALAS 16
-name = '3dqalas_10s_16'
-prep_order = ['T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50']
-
-# 3D QALAS 20
-name = '3dqalas_10s_20'
-prep_order = ['T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep']
-
-
+prep_blocks_dict = {
+    'jaubert': ['TI12', 'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI300',     'noPrep', 'T2prep40', 'T2prep80', 'T2prep120', 'TI12', 'noPrep'],
+    'kvernby': ['T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50'],
+    'hamilton': ['TI21', 'noPrep', 'T2prep40', 'T2prep80', 'TI100', 'noPrep', 'T2prep40', 'T2prep80', 'TI250', 'noPrep', 'T2prep40', 'T2prep80', 'TI400', 'noPrep', 'T2prep40', 'T2prep80']
+}
 #%%
+which = 'hamilton'
+beats = 16
+shots = 35
+const_tr = 5.7
+te = 1.
+prep_blocks = prep_blocks_dict[which]
+phase_inc = 0.
 total_dur = 1e4
-
-acq_block = AcquisitionBlock(np.full(35, 15.), np.full(35, 5.7), TE=1.4)
-
-waittimes = np.concatenate((np.full(len(prep_order)-1, total_dur - np.sum([BLOCKS[prep]['ti'] + BLOCKS[prep]['t2te'] + sum(acq_block.tr) for prep in prep_order]))/(len(prep_order)-1), [0]))
-
-mrf_seq = MRFSequence(prep_order, waittimes)
+name = f'{which}_{total_dur/1e3:.0f}s_{beats}_phinc{phase_inc:.0f}deg'
 
 #%%
-visualize_sequence(mrf_seq, acq_block)
+prep_order = np.concatenate((np.tile(prep_blocks, reps=beats//len(prep_blocks)), prep_blocks[:beats%len(prep_blocks)]))
+waittimes = np.full(beats-1, total_dur - np.sum([BLOCKS[prep]['ti'] + BLOCKS[prep]['t2te'] + shots*const_tr for prep in prep_order]))/(beats-1)
+prep = [BLOCKS[name]['prep'] for name in prep_order]
+ti = [BLOCKS[name]['ti'] for name in prep_order]
+t2te = [BLOCKS[name]['t2te'] for name in prep_order]
+fa = np.full(beats * shots, 15.)
+tr = np.full(beats*shots, 0)
+for ii in range(len(waittimes)):
+    tr[(ii+1)*shots-1] += waittimes[ii]*1e3
+ph = phase_inc*np.arange(beats*shots).cumsum()
+mrf_seq = MRFSequence(beats, shots, fa, tr, ph, prep, ti, t2te, const_tr, te)
 
-# %%
-FA_FISP = np.tile(acq_block.fa, len(mrf_seq.PREP))
-
-TR_FISP = np.zeros_like(FA_FISP)
-for i, waittime in enumerate(waittimes):
-    TR_FISP[len(acq_block.tr)*(i+1)-1] = waittime * 1e3
-
-PH_FISP = np.zeros_like(FA_FISP)
+#%%
+visualize_sequence(mrf_seq, show_fa=True)
 
 #%% save lists
 savepath = Path(f'/home/tomgr/Documents/shared_files/{name}')
 savepath.mkdir(exist_ok=True)
-np.savetxt(savepath/'PREP_FISP.txt', mrf_seq.PREP, fmt='%i')
-np.savetxt(savepath/'TI_FISP.txt', mrf_seq.TI, fmt='%f')
-np.savetxt(savepath/'T2TE_FISP.txt', mrf_seq.T2TE, fmt='%f')
-np.savetxt(savepath/'FA_FISP.txt', FA_FISP, fmt='%f')
-np.savetxt(savepath/'TR_FISP.txt', TR_FISP, fmt='%f')
-np.savetxt(savepath/'PH_FISP.txt', PH_FISP, fmt='%f')
+np.savetxt(savepath/'PREP_FISP.txt', mrf_seq.prep, fmt='%i')
+np.savetxt(savepath/'TI_FISP.txt', mrf_seq.ti, fmt='%f')
+np.savetxt(savepath/'T2TE_FISP.txt', mrf_seq.t2te, fmt='%f')
+np.savetxt(savepath/'FA_FISP.txt', mrf_seq.fa, fmt='%f')
+np.savetxt(savepath/'TR_FISP.txt', mrf_seq.tr, fmt='%f')
+np.savetxt(savepath/'PH_FISP.txt', mrf_seq.ph, fmt='%f')
 
 # %%
