5c6bc8304bb3605e76424245e54a01ab0413ecfb
Tom Griesler <tomgr@umich.edu>
2023-12-13 16:17:28 -0500

    restructure and adapt to t1rho and new parallelization implementation

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
 #%%
-from optimize_sequence import optimize_sequence as opt_seq_nonpar
-from optimize_sequence_parallel import optimize_sequence as opt_seq_par
+from optimize_sequence_parallel import optimize_sequence
 from abdominal_tools import store_optimization, get_gitbranch, get_githash, RESULTSPATH
 
 #%% Define optimization target
 costfunction = 'crlb'
-target_t1 = 660.
-target_t2 = 40.
-# target_t1 = [150, 828]
-# target_t2 = [20, 72]
-target_m0 = 1.
-
-shots = 35
-const_fa = [0., 10., 25.]
-# const_fa = [5., 10., 15., 20., 25.]
-const_tr = 5.7
-te = 1.
-total_dur = 1e4
-
-prep_modules = ['noPrep', 'TI21', 'TI100', 'TI250', 'TI400', 'T2prep40', 'T2prep80', 'T2prep120']
-prep_module_weights = [1, 1/4, 1/4, 1/4, 1/4, 1/3, 1/3, 1/3]
-min_num_preps = 8
-# prep_modules = ['noPrep', 'TI21', 'T2prep40', 'T2prep80', 'T2prep120']
-# prep_module_weights = [1, 1, 1/3, 1/3, 1/3]
-# min_num_preps = None
-
+optimize_positions = False
 n_iter_max = 1e6
+shots = 47
+const_fa = [0., 10., 25.]
+const_tr = 5.4
+te = 1.
+total_dur = 15e3
+
+parallel = True
 num_junks = 1e3
 num_workers = 16
 
+target_t1 = 660.
+target_t2 = 40.
+target_t1rho = 55
+target_m0 = 1.
+
 inv_eff = 0.95
 delta_B1 = 1.
-phase_inc = 3.
+phase_inc = 0.
+
+prep_modules = ['noPrep', 'TI21', 'T1rhoprep30', 'T1rhoprep50', 'T1rhoprep60', 'T2prep30', 'T2prep50', 'T2prep80']
+prep_module_weights = [1/5, 1/5, 1/15, 1/15, 1/15, 2/15, 2/15, 2/15]
+min_beats = 15
+max_beats = 15
+
+# prep_modules = ['noPrep', 'TI21', 'TI100', 'TI250', 'TI400', 'T2prep40', 'T2prep80', 'T2prep120']
+# prep_module_weights = [1, 1/4, 1/4, 1/4, 1/4, 1/3, 1/3, 1/3]
+# min_beats = 8
+
+# prep_modules = ['noPrep', 'TI21', 'T2prep40', 'T2prep80', 'T2prep120']
+# prep_module_weights = [1, 1, 1/3, 1/3, 1/3]
+# min_beats = None
 
 #%% Perform optimization
 gitbranch = get_gitbranch()
 githash = get_githash()
 
-sequences = opt_seq_par(costfunction, target_t1, target_t2, target_m0, shots, const_fa, const_tr, te, total_dur, prep_modules, prep_module_weights, min_num_preps, n_iter_max, inv_eff, delta_B1, phase_inc, num_workers=num_workers, num_junks=num_junks)
-
-# sequences = opt_seq_nonpar(costfunction, target_t1, target_t2, target_m0, shots, const_fa, const_tr, te, total_dur, prep_modules, prep_module_weights, min_num_preps, n_iter_max, inv_eff, delta_B1, phase_inc)
+sequences = optimize_sequence(costfunction, target_t1, target_t2, target_t1rho, target_m0 , shots, const_fa, const_tr, te, total_dur, prep_modules, prep_module_weights, min_beats, max_beats, n_iter_max, inv_eff, delta_B1, phase_inc, optimize_positions, parallel, num_workers=num_workers, num_junks=num_junks)
 
 #%%
 prot = {
@@ -54,7 +57,7 @@ prot = {
     'total_dur': total_dur,
     'prep_modules': prep_modules, 
     'prep_module_weights': prep_module_weights,
-    'min_num_preps': min_num_preps,
+    'min_beats': min_beats,
     'n_iter_max': n_iter_max,
     'inv_eff': inv_eff,
     'delta_B1': delta_B1,
@@ -63,11 +66,3 @@ prot = {
     'githash': githash
 }
 store_optimization(RESULTSPATH, sequences, prot)
-
-# #%%
-# from pathlib import Path
-# altpath = Path('/media/tomgr/TOSHIBA EXT/sequences')
-# store_optimization(altpath, sequences, prot)
-# # %%
-
-# %%
