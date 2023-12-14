ee51f9805192b51d7584d6342bd1606314803788
Tom Griesler <tomgr@umich.edu>
2023-12-14 14:30:35 -0500

    update to current implementation and start analyzing t1rho optimizations

--- compare_sequences.py
+++ compare_sequences.py
@@ -1,63 +1,74 @@
 #%%
 import pickle
-import matplotlib.pyplot as plt
 import json
 import numpy as np
+from tqdm import tqdm
 
-from abdominal_tools import RESULTSPATH, create_weightingmatrix, sort_sequences, visualize_sequence, TargetTissue
+from abdominal_tools import RESULTSPATH, create_weightingmatrix, sort_sequences
 
 #%%
-timestamp = '231020_074613'
+timestamp = '231214_070808'
 resultspath = RESULTSPATH/timestamp
 
 with open(resultspath/'sequences.pkl', 'rb') as handle: 
     sequences = pickle.load(handle)
 
-with open(resultspath/'acq_block.pkl', 'rb') as handle: 
-    acq_block = pickle.load(handle)
-
 with open(resultspath/'prot.json', 'r') as handle: 
     prot = json.load(handle)
 
-target_tissue = TargetTissue(prot['target_tissue']['T1'], prot['target_tissue']['T2'], prot['target_tissue']['M0'])
+#%%
+for sequence in tqdm(sequences, total=len(sequences), desc='Decompressing'):
+    sequence.decompress()
 
 #%%
-seqs_sorted_T1T2 = sequences.copy()
-weighting = '1/T1, 1/T2, 0'
-weightingmatrix_T1T2 = create_weightingmatrix(target_tissue, weighting)
-sort_sequences(seqs_sorted_T1T2, weightingmatrix_T1T2)
-
-seqs_sorted_T1 = sequences.copy()
-weighting = '1/T1, 0, 0'
-weightingmatrix_T1 = create_weightingmatrix(target_tissue, weighting)
-sort_sequences(seqs_sorted_T1, weightingmatrix_T1)
-
-seqs_sorted_T2 = sequences.copy()
-weighting = '0, 1/T2, 0'
-weightingmatrix_T2 = create_weightingmatrix(target_tissue, weighting)
-sort_sequences(seqs_sorted_T2, weightingmatrix_T2)
+target_t1 = prot['target_t1']
+target_t2 = prot['target_t2']
+try:
+    target_t1rho = prot['target_t1rho']
+    dims = 4
+except KeyError:
+    target_t1rho = None
+    dims = 3
 
 #%%
-N = 40
+weighting = 'T1'
+weightingmatrix_T1 = create_weightingmatrix(weighting, target_t1, target_t1, target_t1rho, dims)
+seqs_sorted_T1 = sort_sequences(sequences, weightingmatrix_T1)
 
-plt.figure()
+weighting = 'T2'
+weightingmatrix_T2 = create_weightingmatrix(weighting, target_t1, target_t1, target_t1rho, dims)
+seqs_sorted_T2 = sort_sequences(sequences, weightingmatrix_T2)
 
-for i in range(N):
+weighting = 'T1rho'
+weightingmatrix_T1rho = create_weightingmatrix(weighting, target_t1, target_t1, target_t1rho, dims)
+seqs_sorted_T1rho = sort_sequences(sequences, weightingmatrix_T1rho)
 
-    plt.subplot(N, 1, i+1)
+weighting = 'T1, T2, T1rho'
+weightingmatrix_T1T2T1rho = create_weightingmatrix(weighting, target_t1, target_t1, target_t1rho, dims)
+seqs_sorted_T1T2T1rho = sort_sequences(sequences, weightingmatrix_T1T2T1rho)
 
-    visualize_sequence(seqs_sorted_T2[i], acq_block)
-    plt.xlim(0, 10000)
-    ax = plt.gca()
-    ax.set_xticklabels([])
-    ax.set_yticks([])
-    
+#%%
+N = 100
+
+#%%
+weightings = ['T1', 'T2', 'T1rho', 'all']
+seqs_sorted = [seqs_sorted_T1[:N], seqs_sorted_T2[:N], seqs_sorted_T1rho[:N], seqs_sorted_T1T2T1rho[:N]]
+
+results = {name: {} for name in weightings}
+
+for name, seqs in tqdm(zip(weightings, seqs_sorted), total=len(weightings)):
+    results[name]['fa'] = np.mean([np.mean(seq.fa) for seq in seqs])
+    results[name]['n'] = np.mean([len(seq.prep) for seq in seqs])
+    results[name]['n_1'] = np.mean([np.count_nonzero(seq.prep==1) for seq in seqs])
+    results[name]['n_2'] = np.mean([np.count_nonzero(seq.prep==2) for seq in seqs])
+    results[name]['n_3'] = np.mean([np.count_nonzero(seq.prep==3) for seq in seqs])
+
+
 # %%
-np.mean([len(seq.PREP) for seq in seqs_sorted_T1[:40]])
-np.mean([len(seq.PREP) for seq in seqs_sorted_T2[:40]])
-np.mean([len(seq.PREP) for seq in seqs_sorted_T1T2[:40]])
-
-#%%
-np.mean([seq.PREP.count(2) for seq in seqs_sorted_T1T2[:40]])
-
+print('\t', end='')
+for what in results[weightings[0]].keys(): print(what, end='\t')
+for sortby in results.keys():
+    print('\n', sortby, end='\t')
+    for what in results[sortby].keys():
+        print(f'{results[sortby][what]:.2f}', end='\t')
 # %%
