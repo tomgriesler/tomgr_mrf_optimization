8ff49373ed05d2ac8f2d8055af4257d5689abc98
Tom Griesler <tomgr@umich.edu>
2023-12-14 10:00:36 -0500

    remove unused code

--- optimize_sequence_parallel.py
+++ optimize_sequence_parallel.py
@@ -54,40 +54,6 @@ def optimize_sequence_worker(junk_size, args):
 
     for _ in range(junk_size):
         
-        # beats = random.randint(min_beats, max_beats)
-        # n_ex = beats * shots
-
-        # prep_order = random.choices(prep_modules, weights=prep_module_weights, k=beats)
-        # prep = [BLOCKS[name]['prep'] for name in prep_order]
-        # ti = [BLOCKS[name]['ti'] for name in prep_order]
-        # t2te = [BLOCKS[name]['t2te'] for name in prep_order]
-        # tsl = [BLOCKS[name]['tsl'] for name in prep_order]
-
-        # prep_time_tot = sum([BLOCKS[name]['ti'] + BLOCKS[name]['t2te'] + BLOCKS[name]['tsl'] for name in prep_order])
-
-        # if optimize_positions:
-        #     waittime_tot = total_dur - beats*shots*const_tr - prep_time_tot
-        #     waittimes = divide_into_random_floats(waittime_tot, beats-1)
-        # else:
-        #     waittimes = [total_dur/beats-ti[ii]-t2te[ii]-tsl[ii] for ii in range(beats)]
-
-        # fa = np.repeat(random.choices((const_fa), k=beats), shots) if type(const_fa) == list else np.full(beats*shots, const_fa)
-
-        # # Ensure that not all flipangles are zero
-        # if np.count_nonzero(fa) == 0:
-        #     continue
-
-        # tr = np.full(beats*shots, 0)
-
-        # for ii in range(len(waittimes)):
-        #     tr[(ii + 1) * shots - 1] += waittimes[ii] * 1e3
-
-        # ph = phase_inc * np.arange(n_ex).cumsum()
-
-        # mrf_sequence = MRFSequence(beats, shots, fa, tr, ph, prep, ti, t2te, const_tr, te)
-
-        # mrf_sequence.calc_cost(costfunction, target_t1, target_t2, target_m0, inv_eff, delta_B1)
-
         sequence = create_sequence(args)
 
         sequences.append(sequence)
