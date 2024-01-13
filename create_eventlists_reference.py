5c22ed66672a335d5a0ee6152706f292c624b1dd
Tom Griesler <tomgr@umich.edu>
2024-01-13 10:21:43 -0500

    add sydneys prep scheme

--- create_eventlists_reference.py
+++ create_eventlists_reference.py
@@ -11,30 +11,40 @@ prep_blocks_dict = {
     'kvernby': ['T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50', 'TI100', 'noPrep', 'noPrep', 'noPrep', 'T2prep50'],
     'hamilton': ['TI21', 'noPrep', 'T2prep40', 'T2prep80', 'TI100', 'noPrep', 'T2prep40', 'T2prep80', 'TI250', 'noPrep', 'T2prep40', 'T2prep80', 'TI400', 'noPrep', 'T2prep40', 'T2prep80'],
     'hamilton_mod': ['TI21', 'noPrep', 'T2prep40', 'T2prep80'],
-    
+    'sydney': ['TI21', 'noPrep', 'T1rhoprep30', 'T1rhoprep50', 'T1rhoprep60', 'TI21', 'noPrep', 'T2prep30', 'T2prep50', 'T2prep80', 'TI21', 'noPrep', 'T2prep30', 'T2prep50', 'T2prep80']
 }
 #%%
-which = 'hamilton_mod'
-beats = 31
-shots = 35
-const_tr = 5.7
+which = 'sydney'
+beats = 15
+shots = 47
+const_tr = 5.4
 te = 1.
 prep_blocks = prep_blocks_dict[which]
-phase_inc = 3.
-total_dur = 1e4
+phase_inc = 0.
+total_dur = 1.5e4
 name = f'{which}_{total_dur/1e3:.0f}s_{beats}_phinc{phase_inc:.0f}deg'
 
-#%%
+#%% 
 prep_order = np.concatenate((np.tile(prep_blocks, reps=beats//len(prep_blocks)), prep_blocks[:beats%len(prep_blocks)]))
-waittimes = np.full(beats-1, total_dur - np.sum([BLOCKS[prep]['ti'] + BLOCKS[prep]['t2te'] + shots*const_tr for prep in prep_order]))/(beats-1)
 prep = [BLOCKS[name]['prep'] for name in prep_order]
 ti = [BLOCKS[name]['ti'] for name in prep_order]
 t2te = [BLOCKS[name]['t2te'] for name in prep_order]
+tsl = [BLOCKS[name]['tsl'] for name in prep_order]
 fa = np.full(beats * shots, 15.)
-tr = np.full(beats*shots, 0)
+ph = phase_inc*np.arange(beats*shots).cumsum()
+
+#%% equal distance between blocks
+waittimes = np.full(beats-1, total_dur - np.sum([BLOCKS[prep]['ti'] + BLOCKS[prep]['t2te'] + BLOCKS[prep]['tsl'] + shots*const_tr for prep in prep_order]))/(beats-1)
+tr = np.zeros(beats*shots)
 for ii in range(len(waittimes)):
     tr[(ii+1)*shots-1] += waittimes[ii]*1e3
-ph = phase_inc*np.arange(beats*shots).cumsum()
+
+#%% equal distance between start of blocks
+tr = np.zeros(beats*shots)
+for ii in range(beats):
+    tr[(ii+1)*shots-1] += 1e6 - (ti[ii] + t2te[ii] + tsl[ii] + const_tr*shots)*1e3
+
+#%%
 mrf_seq = MRFSequence(beats, shots, fa, tr, ph, prep, ti, t2te, const_tr, te)
 
 #%%
@@ -45,7 +55,7 @@ savepath = Path(f'/home/tomgr/Documents/abdominal/data/sequences/{name}/textfile
 savepath.mkdir(exist_ok=True, parents=True)
 np.savetxt(savepath/'PREP_FISP.txt', mrf_seq.prep, fmt='%i')
 np.savetxt(savepath/'TI_FISP.txt', mrf_seq.ti, fmt='%f')
-np.savetxt(savepath/'T2TE_FISP.txt', mrf_seq.t2te, fmt='%f')
+np.savetxt(savepath/'T2TE_FISP.txt', mrf_seq.t2te + mrf_seq.tsl, fmt='%f')
 np.savetxt(savepath/'FA_FISP.txt', mrf_seq.fa, fmt='%f')
 np.savetxt(savepath/'TR_FISP.txt', mrf_seq.tr, fmt='%f')
 np.savetxt(savepath/'PH_FISP.txt', mrf_seq.ph, fmt='%f')
