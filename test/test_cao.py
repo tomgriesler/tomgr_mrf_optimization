4de8364e415dcea0a8beaa88de1dfd0433218bb6
Tom Griesler <tomgr@umich.edu>
2024-03-18 16:39:15 -0400

    minor changes

--- test/test_cao.py
+++ test/test_cao.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from utils.abdominal_tools import MRFSequence
+from utils.visualization import visualize_sequence
 
 #%%
 beats = 1
@@ -16,23 +17,18 @@ prep = [1]
 ti = [20]
 t2te = [0]
 ph = np.zeros_like(fa)
-ph[::2] = 180
 
 tr_offset = 12.
+ph[::2] = 180
 
+# %% not alternating
 mrf_seq = MRFSequence(beats, shots, fa, tr, ph, prep, ti, t2te, tr_offset, 1.4)
 
-#%%
-t1 = 660.
-t2 = 40.
-m0 = 1.
+plt.figure()
 
-# %%
-signal_fisp = mrf_seq.calc_signal_fisp(t1, t2, m0, inv_eff=1., return_result=True)
-signal_bssfp = mrf_seq.calc_signal_bssfp(t1, t2, m0, inv_eff=1., return_result=True)
-
-plt.plot(-np.imag(signal_fisp), label='fisp')
-plt.plot(-np.imag(signal_bssfp), label='bssfp')
+for t1, t2 in zip([4000, 800, 150], [600, 70, 20]):
+    signal = mrf_seq.calc_signal_fisp(t1, t2, 1, inv_eff=1., return_result=True)
+    plt.plot(-np.imag(signal), label=f'%i, %i' %(t1, t2))
 
 plt.legend()
 # %%
